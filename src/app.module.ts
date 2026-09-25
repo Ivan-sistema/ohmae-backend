7ee1b4c589785c045664e53e3f81111a252c7bb2
Ivan Francisco <ivan.santos@helptask.com.br>
2026-09-24 21:53:59 -0300

    [feature/vinculos] - Create vinculo entre mae e filho

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CriancasModule } from './modules/criancas/criancas.module.js'; // Novo import
+import { CriancasModule } from './modules/criancas/criancas.module.js';
 import { UsuariosModule } from './modules/usuarios/usuarios.module.js';
+import { VinculosModule } from './modules/vinculos/vinculos.module.js'; // Novo import
 
 @Module({
-  imports: [UsuariosModule, CriancasModule], // Plugado aqui!
+  imports: [UsuariosModule, CriancasModule, VinculosModule], // Plugado com sucesso!
   controllers: [],
   providers: [],
 })
